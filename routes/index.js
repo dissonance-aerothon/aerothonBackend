534487784cd22592d5db940ebee3ee0b2292b9a9
Amir Khan <amirkhan@Amirs-MacBook-Air.local>
2023-02-26 14:53:06 +0530

    fds

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { User } from '../utils/collection';
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', async function (req, res, next) {
-});
-
-module.exports = router;
