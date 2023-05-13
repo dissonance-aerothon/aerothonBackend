269915045fb1e3c2d02d514d56512c287867efb2
Amir Khan <amir.khan@wolkus.com>
2023-05-13 12:56:45 +0530

    fixed a bug

--- utils/validateRoles.js
+++ utils/validateRoles.js
@@ -1,17 +1,11 @@
 import { User } from './collection';
 
-const validateRole = async (roles) => {
-  console.log(roles);
+const validateRole = async (roles, email) => {
   const userRoles = await User.findOne(
-    { email: req.user.email },
-    { roles: 1, _id: 0 }
+    { email },
+    { roles: { $in: roles }, _id: 0 }
   );
-  console.log(userRoles);
-  roles.forEach((role) => {
-    if (userRoles && userRoles.roles && userRoles.roles.indexOf(role) > -1) {
-      return true;
-    }
-  });
+  if (userRoles) return true;
   return false;
 };
 
